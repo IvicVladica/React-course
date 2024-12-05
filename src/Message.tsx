67ecc36575088dde0f65e3e98cf3097e68bc26fb
IvicVladica <ivicvladica@gmail.com>
2024-12-05 16:12:04 +0100

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// Pascal casing
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Vlada';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message; 
