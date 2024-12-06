03fe1a4d7aef1ca744d8344af49fc9537bb6a1d8
IvicVladica <ivicvladica@gmail.com>
2024-12-06 14:44:16 +0100

    Building a Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+// function App() {
+//   let items = ["Beograd", "Nis", "Novi Sad", "Kragujevac", "Kursumlija"];
+
+//   const hanleSelectItem = (item: string) => {
+//     console.log(item);
+//   }
+//   return (
+//     <div>
+//       <ListGroup items={items} heading="Cities" onSelectItem={hanleSelectItem}/>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Alert>
+//         Hello <span>World</span>
+//       </Alert>
+//     </div>
+//   );
+// }
 
 function App() {
-  let items = ["Beograd", "Nis", "Novi Sad", "Kragujevac", "Kursumlija"];
-
-  const hanleSelectItem = (item: string) => {
-    console.log(item);
-  }
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={hanleSelectItem}/>
+      <Button color="success" onClick={() => console.log('Clicked')}>
+        My Button
+      </Button>
     </div>
   );
 }
