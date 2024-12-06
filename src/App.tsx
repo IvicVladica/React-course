429e92892e16838472f49b83207bab966006b974
IvicVladica <ivicvladica@gmail.com>
2024-12-06 08:33:34 +0100

    Passing data via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
-function App () {
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["Beograd", "Nis", "Novi Sad", "Kragujevac", "Kursumlija"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities"/>
+    </div>
+  );
 }
 
 export default App;
